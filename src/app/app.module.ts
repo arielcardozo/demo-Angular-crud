4e73cd51de996016c99c0b8fe4b43a0273545a65
Ariel Cardozo <ariel.card@gmail.com>
2021-01-11 09:27:25 +0100

    Material and Flex configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
-import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AppRoutingModule} from './app-routing.module';
+
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
 import {AgGridModule} from 'ag-grid-angular';
+import { AlertComponent } from './shared/alert/alert.component';
+import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthComponent} from './auth/auth.component';
+import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { DropdownDirective } from './shared/dropdown.directive';
 import {EmployeeDetailsComponent} from './components/employee-details/employee-details.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import {AuthComponent} from './auth/auth.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
+import {HeaderComponent} from './header/header.component';
 import {LoadingSpinnerComponent} from './loading-spinner/loading-spinner/loading-spinner.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import { MaterialModule } from './material.module';
+import { NgModule } from '@angular/core';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
 import {TutorialService} from './services/tutorial.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+
 // import 'ag-grid-enterprise';
 
 
@@ -42,9 +45,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    AgGridModule.withComponents([]),
-    BrowserAnimationsModule
-
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [TutorialService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,15 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+
+const modules = [
+  MatButtonModule, MatToolbarModule, MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule { }
